5bcdc9e4f82582864560ac5bdab419ac5eb7a3e6
Andres2D <andres2d1997.1206@gmail.com>
2023-04-28 18:39:12 -0500

    Add bassic logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import styles from './App.module.scss';
 
 const App = () => {
   return (
-    <section className={styles.section}>
-      <Search />
-      <Card 
-        word="wallpaper"
-        phonetic="/ˈwɔːlˌpeɪ.pər/ noun"
-        meaning="paper that is pasted in vertical
-        strips over the walls of a room to
-        provide a decorative surface."
-      />
-    </section>
+    <>
+      <h1 className={styles.logo}>meaningful.app</h1>
+      <section className={styles.section}>
+        <Search />
+        <Card 
+          word="wallpaper"
+          phonetic="/ˈwɔːlˌpeɪ.pər/ noun"
+          meaning="paper that is pasted in vertical
+          strips over the walls of a room to
+          provide a decorative surface."
+        />
+      </section>
+    </>
   )
-}
+};
 
 export default App;
