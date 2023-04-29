faa553c7d1a466222dcd9b99413ad35873d08a1a
Andres2D <andres2d1997.1206@gmail.com>
2023-04-29 10:24:59 -0500

    Add search results elements

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,8 +1,31 @@
 import styles from './search.module.scss';
+import { useState } from 'react';
 
 const Search = () => {
+
+  const [searching, setSearching] = useState(false);
+
+  const handleSearch = () => {
+    setSearching(true);
+  };
+  
   return (
-    <input className={styles.search} type="text" placeholder='wallpaper' />
+    <div className={styles.search}>
+      <input 
+        className={styles.input} 
+        type="text" 
+        placeholder='wallpaper'
+        onChange={handleSearch}
+      />
+      {
+        searching &&
+        <ul className={styles.results}>
+          <li className={styles.item}>test</li>
+          <li className={styles.item}>test 3</li>
+          <li className={styles.item}>test 4</li>
+        </ul>
+      }
+    </div>
   )
 };
 
