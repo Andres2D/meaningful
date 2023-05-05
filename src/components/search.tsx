99e0c570be45b65d5be8bcf92aa1bf4a60a59af0
Andres2D <andres2d1997.1206@gmail.com>
2023-05-05 12:14:24 -0500

    Search suggestion word and search specific word

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,24 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import styles from './search.module.scss';
+import { Definition } from '../interfaces/dictionary';
 
 const Search = () => {
 
   const [searching, setSearching] = useState(false);
+  const [inputWord, setInputWord] = useState('');
+  const [selectedWord, setSelectedWord] = useState('');
+
   const { data, refetch } = useQuery({
+    queryKey: ['getSuggestions'],
+    enabled: false,
+    queryFn: () => 
+      axios
+      .get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${inputWord}?key=${import.meta.env.VITE_DICTIONARY_KEY}`)
+      .then((response: Definition[] | string[] | any) => {
+        const definitions = Array.isArray(response.data) ? response.data : [];
+        if(definitions.length === 0) { return [] }
+        if(typeof definitions[0] === 'string' || definitions[0] instanceof String) {
+          return definitions.splice(0, 4);
+        } else {
+          const word = definitions[0] as Definition;
+          return [word.meta.id.replace(':1', '')];
+        }
+      })
+  });
+
+  const { data: wordDefinition, refetch: singleFetch } = useQuery({
     queryKey: ['getMeaning'],
     enabled: false,
     queryFn: () => 
-      fetch('https://api.dictionaryapi.dev/api/v2/entries/en/wall')
+      axios
+      .get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${selectedWord}?key=${import.meta.env.VITE_DICTIONARY_KEY}`)
       .then((response: any) => {
-        console.log(response);
+        return response.data[0];        
       })
-  })
+  });
 
-  const handleSearch = () => {
+  useEffect(() => {
+    if(selectedWord !== '') {
+      singleFetch();
+    }
+  }, [selectedWord, singleFetch]);
+
+  const handleSearch = (event: any) => {
+    setInputWord(event.target.value);
     setSearching(true);
     refetch();
   };
+  
+  const handleSearchWord = (wordSelected: string) => {
+    setSelectedWord(wordSelected);
+    setSearching(false);
+  }
   
   return (
     <div className={styles.search}>
@@ -31,9 +67,14 @@ const Search = () => {
       {
         searching &&
         <ul className={styles.results}>
-          <li className={styles.item}>test</li>
-          <li className={styles.item}>test 3</li>
-          <li className={styles.item}>test 4</li>
+          {data?.map((definition: string, index: number) => 
+            <li 
+              key={index} 
+              className={styles.item}
+              onClick={() => handleSearchWord(definition)}>
+                {definition}
+            </li>
+          )}
         </ul>
       }
     </div>
--- src/interfaces/dictionary.ts
+++ src/interfaces/dictionary.ts
@@ -3,3 +3,63 @@ export interface Word {
   phonetic: string;
   meaning: string;
 }
+
+export interface Definition {
+  meta:     Meta;
+  hom:      number;
+  hwi:      Hwi;
+  fl:       string;
+  def:      Def[];
+  date:     string;
+  shortdef: string[];
+  ins?:     In[];
+}
+
+export interface Def {
+  sseq: Array<Array<Array<SseqClass | string>>>;
+  vd?:  string;
+}
+
+export interface SseqClass {
+  sn?: string;
+  dt:  Array<Array<DtClass[] | string>>;
+}
+
+export interface DtClass {
+  t:  string;
+  aq: Aq;
+}
+
+export interface Aq {
+  auth: string;
+}
+
+export interface Hwi {
+  hw:   string;
+  prs?: PR[];
+}
+
+export interface PR {
+  mw:    string;
+  sound: Sound;
+}
+
+export interface Sound {
+  audio: string;
+  ref:   string;
+  stat:  string;
+}
+
+export interface In {
+  if: string;
+}
+
+export interface Meta {
+  id:        string;
+  uuid:      string;
+  sort:      string;
+  src:       string;
+  section:   string;
+  stems:     string[];
+  offensive: boolean;
+}
